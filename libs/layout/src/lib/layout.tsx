79f7195889456342ae10fce77bc268caae002b51
kzoeps <kzoepa@gmail.com>
2022-01-03 22:03:02 +0600

    feat: install ant design icons

--- libs/layout/src/lib/layout.tsx
+++ libs/layout/src/lib/layout.tsx
@@ -1,13 +1,35 @@
 import './layout.module.scss';
+import { Layout as AntDLayout, Menu } from 'antd';
+import React from 'react';
 
 /* eslint-disable-next-line */
 export interface LayoutProps {}
 
+const { Sider } = AntDLayout;
+
 export function Layout(props: LayoutProps) {
   return (
-    <div>
-      <h1>Welcome to layout!</h1>
-    </div>
+    <AntDLayout>
+      <Sider
+      breakpoint="lg"
+      collapsedWidth="0">
+        <div>LOGO HERE</div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+          <Menu.Item key="4" icon={<UserOutlined />}>
+            nav 4
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    </AntDLayout>
   );
 }
 
