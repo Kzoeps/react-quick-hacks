931f05cb0b3976de35c51699f1012229b76d15b9
kzoeps <kzoepa@gmail.com>
2022-01-23 18:44:53 +0600

    fix(layout): fix the menu.item issue from layout

--- apps/mbob-scanner/src/app/app.tsx
+++ apps/mbob-scanner/src/app/app.tsx
@@ -2,13 +2,15 @@ import './app.module.scss';
 import { Button } from 'antd';
 import React from 'react';
 import { Layout } from '@react-quick-hacks/layout';
+import Shell from './shell/shell';
+import Login from './routes/login/login';
 
 export function App() {
   return (
     <>
-      <Layout>
-        <Button type="primary" ghost>Hello</Button>
-      </Layout>
+      <Shell>
+        <p>Soy dora</p>
+      </Shell>
     </>
 
   );
--- libs/layout/src/lib/layout.tsx
+++ libs/layout/src/lib/layout.tsx
@@ -1,12 +1,12 @@
 import './layout.module.scss';
-import { Breadcrumb, Button, Layout as AntDLayout, Menu } from 'antd';
-import React, { FC, ReactElement, useState } from 'react';
+import { Button, Layout as AntDLayout, Menu } from 'antd';
+import React, { ReactElement, useState } from 'react';
 import HktDrawer from './components/hkt-drawer/hkt-drawer';
 import { MenuUnfoldOutlined } from '@ant-design/icons';
 
 /* eslint-disable-next-line */
 export interface LayoutProps {
-  children: ReactElement
+  children: ReactElement;
 }
 
 const { Header, Content, Footer } = AntDLayout;
@@ -22,8 +22,10 @@ export function Layout({children}: LayoutProps) {
       <Header>
         <div className='logo' />
         <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
-          <Button shape='circle' icon={<MenuUnfoldOutlined />} onClick={() => setShowDrawer(true)} ghost/>
-          <HktDrawer showDrawer={showDrawer} onDrawerClose={onDrawerClose} />
+          <Menu.Item>
+            <Button shape='circle' icon={<MenuUnfoldOutlined />} onClick={() => setShowDrawer(true)} ghost />
+            <HktDrawer showDrawer={showDrawer} onDrawerClose={onDrawerClose} />
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: '0 50px' }}>
