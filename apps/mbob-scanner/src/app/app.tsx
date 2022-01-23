7d42778cc520826d9630fdc06e6d3adadc74a267
kzoeps <kzoepa@gmail.com>
2022-01-23 21:35:14 +0600

    chore: add more configs to typescript airbnb

--- apps/mbob-scanner/src/app/app.tsx
+++ apps/mbob-scanner/src/app/app.tsx
@@ -1,5 +1,4 @@
 import './app.module.scss';
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './routes';
 import Shell from './shell/shell';
