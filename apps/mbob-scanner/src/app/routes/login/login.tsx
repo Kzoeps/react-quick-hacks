1cf569263d779579ed44723106568cdca602f33a
kzoeps <kzoepa@gmail.com>
2022-01-30 14:27:24 +0600

    feat(use phone verify): setup up otp sending

--- apps/mbob-scanner/src/app/routes/login/login.tsx
+++ apps/mbob-scanner/src/app/routes/login/login.tsx
@@ -1,5 +1,6 @@
 import './login.module.scss';
 import { LoginForm } from '@react-quick-hacks/auth';
+import { usePhoneVerify } from '@react-quick-hacks/firebase-auth';
 
 /* eslint-disable-next-line */
 export interface LoginProps {
@@ -7,6 +8,7 @@ export interface LoginProps {
 
 export function Login(props: LoginProps) {
   const temp = () => console.log('ufsl');
+  const verifyPhone = usePhoneVerify();
   return (
     <>
       <LoginForm onSubmit={temp}/>
--- libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
+++ libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
@@ -1,29 +1,47 @@
 import { notification } from 'antd';
 import './use-phone-verfiy.hook.module.scss';
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from 'firebase/auth';
+import { ApplicationVerifier } from '@firebase/auth-types';
 import NotificationTypeEnum from '../../enums/firebase-auth.enum';
 
-/* eslint-disable-next-line */
-export interface UsePhoneVerifyHookProps {}
 export interface UsePhoneVerify {
   sendVerification: (phoneNumber: string) => Promise<void>;
 }
 
-export function usePhoneVerify(props: UsePhoneVerifyHookProps): UsePhoneVerify {
+export function usePhoneVerify(): UsePhoneVerify {
+  const auth = getAuth();
+  const appVerifier = new RecaptchaVerifier('sign-in',{
+    size: 'invisible',
+    callback: (response: string) => {
+      console.log(response);
+    },
+  }, auth)
 
   const showNotification = (message: string, type: NotificationTypeEnum, description?: string) => {
     notification[type]({
       message,
       description
-    })
-  }
+    });
+  };
 
   const sendVerification = async (phoneNumber: string) => {
-    if (!phoneNumber) showNotification('Phone Number Required', NotificationTypeEnum.Error);
-  }
+    if (!phoneNumber) {
+      showNotification('Phone Number Required', NotificationTypeEnum.Error);
+      return;
+    }
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then(() => {
+        showNotification('Verification Code Sent', NotificationTypeEnum.Success);
+      })
+      .catch(error => {
+        showNotification(error.message, NotificationTypeEnum.Error);
+      });
+
+  };
 
   return ({
     sendVerification
-  })
+  });
 }
 
 export default usePhoneVerify;
