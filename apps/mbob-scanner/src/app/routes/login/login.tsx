53b435d006854d83b99cac1052ac355ad901b0cc
kzoeps <kzoepa@gmail.com>
2022-01-30 16:28:19 +0600

    feat(phone verify): set up hook to send verification

--- apps/mbob-scanner/src/app/routes/login/login.tsx
+++ apps/mbob-scanner/src/app/routes/login/login.tsx
@@ -1,17 +1,21 @@
 import './login.module.scss';
 import { LoginForm } from '@react-quick-hacks/auth';
 import { usePhoneVerify } from '@react-quick-hacks/firebase-auth';
+import app from '../../firebase-config';
 
 /* eslint-disable-next-line */
 export interface LoginProps {
 }
 
 export function Login(props: LoginProps) {
-  const temp = () => console.log('ufsl');
-  const verifyPhone = usePhoneVerify();
+  const verifyPhone = usePhoneVerify(app);
+  const temp = async () => {
+    await verifyPhone.sendVerification('+97517123456')
+  }
   return (
     <>
       <LoginForm onSubmit={temp}/>
+      <div id="recaptcha-container" />
     </>
   );
 }
--- libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
+++ libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
@@ -2,21 +2,16 @@ import { notification } from 'antd';
 import './use-phone-verfiy.hook.module.scss';
 import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from 'firebase/auth';
 import { ApplicationVerifier } from '@firebase/auth-types';
+import { FirebaseApp } from 'firebase/app';
 import NotificationTypeEnum from '../../enums/firebase-auth.enum';
 
 export interface UsePhoneVerify {
   sendVerification: (phoneNumber: string) => Promise<void>;
 }
 
-export function usePhoneVerify(): UsePhoneVerify {
-  const auth = getAuth();
-  const appVerifier = new RecaptchaVerifier('sign-in',{
-    size: 'invisible',
-    callback: (response: string) => {
-      console.log(response);
-    },
-  }, auth)
-
+export function usePhoneVerify(app: FirebaseApp): UsePhoneVerify {
+  console.log(app);
+  const auth = getAuth(app);
   const showNotification = (message: string, type: NotificationTypeEnum, description?: string) => {
     notification[type]({
       message,
@@ -24,21 +19,31 @@ export function usePhoneVerify(): UsePhoneVerify {
     });
   };
 
+  const generateRecaptcha = () => {
+     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+     // @ts-ignore
+     window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+       size: 'invisible',
+       callback: (response: unknown) => {
+         console.log(response);
+       }
+     }, auth);
+  }
+
   const sendVerification = async (phoneNumber: string) => {
     if (!phoneNumber) {
       showNotification('Phone Number Required', NotificationTypeEnum.Error);
       return;
     }
-    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    generateRecaptcha()
+    signInWithPhoneNumber(auth, phoneNumber, window['recaptchaVerifier'])
       .then(() => {
         showNotification('Verification Code Sent', NotificationTypeEnum.Success);
       })
       .catch(error => {
         showNotification(error.message, NotificationTypeEnum.Error);
       });
-
   };
-
   return ({
     sendVerification
   });
