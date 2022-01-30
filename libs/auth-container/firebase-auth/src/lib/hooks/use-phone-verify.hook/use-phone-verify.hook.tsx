8c38f708520cf68d2a03d2663f0bc32b46a9452a
kzoeps <kzoepa@gmail.com>
2022-01-30 16:37:15 +0600

    feat(use phone verify): add function to confirm otp

--- libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
+++ libs/auth-container/firebase-auth/src/lib/hooks/use-phone-verify.hook/use-phone-verify.hook.tsx
@@ -1,17 +1,19 @@
 import { notification } from 'antd';
 import './use-phone-verfiy.hook.module.scss';
 import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from 'firebase/auth';
-import { ApplicationVerifier } from '@firebase/auth-types';
+import { ApplicationVerifier, ConfirmationResult } from '@firebase/auth-types';
 import { FirebaseApp } from 'firebase/app';
+import { useState } from 'react';
 import NotificationTypeEnum from '../../enums/firebase-auth.enum';
 
 export interface UsePhoneVerify {
   sendVerification: (phoneNumber: string) => Promise<void>;
+  confirmCode: (otp: string) => Promise<void>;
 }
 
 export function usePhoneVerify(app: FirebaseApp): UsePhoneVerify {
-  console.log(app);
   const auth = getAuth(app);
+  const [confirmationResult, setConfirmationResult] = useState<ConfirmationResult | undefined>(undefined);
   const showNotification = (message: string, type: NotificationTypeEnum, description?: string) => {
     notification[type]({
       message,
@@ -24,9 +26,7 @@ export function usePhoneVerify(app: FirebaseApp): UsePhoneVerify {
      // @ts-ignore
      window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
        size: 'invisible',
-       callback: (response: unknown) => {
-         console.log(response);
-       }
+       callback: (response: unknown) => undefined
      }, auth);
   }
 
@@ -36,16 +36,34 @@ export function usePhoneVerify(app: FirebaseApp): UsePhoneVerify {
       return;
     }
     generateRecaptcha()
-    signInWithPhoneNumber(auth, phoneNumber, window['recaptchaVerifier'])
-      .then(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    signInWithPhoneNumber(auth, phoneNumber, window.recaptchaVerifier)
+      .then((result) => {
+        setConfirmationResult(result as unknown as ConfirmationResult);
         showNotification('Verification Code Sent', NotificationTypeEnum.Success);
       })
       .catch(error => {
         showNotification(error.message, NotificationTypeEnum.Error);
       });
   };
+
+  const confirmCode = async (otp: string): Promise<void> => {
+    if (confirmationResult) {
+      try {
+        await confirmationResult?.confirm(otp);
+        showNotification('Verification Successful', NotificationTypeEnum.Success);
+      } catch (error) {
+        showNotification(error.message, NotificationTypeEnum.Error);
+      }
+    } else {
+      showNotification('Oops something went wrong', NotificationTypeEnum.Error);
+    }
+  };
+
   return ({
-    sendVerification
+    sendVerification,
+    confirmCode
   });
 }
 
