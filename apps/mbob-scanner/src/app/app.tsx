d98f0b5405c8959acc3e1a74dd6906eea3ebc9bc
kzoeps <kzoepa@gmail.com>
2022-02-08 14:31:48 +0600

    feat(sign up): created route for sign up

--- apps/mbob-scanner/src/app/app.tsx
+++ apps/mbob-scanner/src/app/app.tsx
@@ -2,6 +2,7 @@ import './app.module.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { RoutesEnum } from './enums/routes-enum';
 import { Login } from './routes';
 import Shell from './shell/shell';
 
@@ -11,7 +12,8 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Shell/>}>
-            <Route path="login" element={<Login/>}/>
+            <Route path={RoutesEnum.login} element={<Login/>}/>
+            <Route path={RoutesEnum.login} element={<Login/>}/>
             <Route path="" element={<Login/>}/>
           </Route>
         </Routes>
