ce6b9b525660cda7b5139ee288ff8c84cc064b5d
kzoeps <kzoepa@gmail.com>
2022-02-12 01:55:30 +0600

    feat(usetesseract): recognize text from images
    also create a loader function to load the worker

--- apps/mbob-scanner/src/app/hooks/useTesseract.tsx
+++ apps/mbob-scanner/src/app/hooks/useTesseract.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 
 export interface TesseractOptions {
   readImageText: (image: File) => Promise<string | undefined>;
+  loadWorker: () => Promise<void>;
 }
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface UseTesseractProps{
@@ -13,11 +14,19 @@ export const useTesseract = (props: UseTesseractProps): TesseractOptions => {
   const worker = createWorker({
     logger: m => console.log(m),
   });
-  const readImageText = async (image: File): Promise<string | undefined> => {
+
+  const loadWorker = async (): Promise<void> => {
     try {
       await worker.load();
       await worker.loadLanguage('eng');
       await worker.initialize('eng');
+    } catch (error) {
+      showNotification(error.message, NotificationTypeEnum.error);
+    }
+  }
+
+  const readImageText = async (image: File): Promise<string | undefined> => {
+    try {
       const { data: { text } } = await worker.recognize(image);
       return text;
     } catch (error) {
@@ -32,6 +41,7 @@ export const useTesseract = (props: UseTesseractProps): TesseractOptions => {
 
   return {
     readImageText,
+    loadWorker
   };
 }
 
--- apps/mbob-scanner/src/app/routes/dashboard/dashboard.tsx
+++ apps/mbob-scanner/src/app/routes/dashboard/dashboard.tsx
@@ -1,18 +1,26 @@
 import { DatabaseTwoTone, FileAddTwoTone, ScanOutlined } from '@ant-design/icons';
 import { HacketUpload } from '@react-quick-hacks/ui-kit';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UploadFileStatus } from 'antd/es/upload/interface';
 import ItemBox from '../../components/item-box/item-box';
+import useTesseract from '../../hooks/useTesseract';
 
 /* eslint-disable-next-line */
 export interface DashboardProps {}
 
 export function Dashboard(props: DashboardProps) {
   const [uploadStatus, setUploadStatus] = useState<UploadFileStatus | undefined>(undefined);
+  const tesseract = useTesseract({});
+  useEffect(() => {
+    tesseract.loadWorker();
+  }, [])
   return (
     <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
       <HacketUpload accept="image/*"
-                    customRequest={(file) => {console.log(file)}} uploadInterface={<ItemBox label='Scan' value='scan'
+                    customRequest={(file) => {
+                      tesseract.readImageText(file.file as File);
+                      console.log(file)
+                    }} uploadInterface={<ItemBox label='Scan' value='scan'
                                               icon={<ScanOutlined
                                                 style={{ fontSize: '30px', color: '#1890ff' }} />} />} />
       <ItemBox label='Records' value='records' icon={<DatabaseTwoTone style={{ fontSize: '30px' }} />} />
