b51f798667401aa728cceb0535425d70a69efeca
kzoeps <kzoepa@gmail.com>
2022-02-17 20:13:07 +0600

    feat(entry form): create submit button

--- apps/mbob-scanner/src/app/components/entry-form/entry-form.tsx
+++ apps/mbob-scanner/src/app/components/entry-form/entry-form.tsx
@@ -1,15 +1,17 @@
 import './entry-form.module.scss';
 import { Form, Formik } from 'formik';
 import { HacketInput } from '@react-quick-hacks/ui-kit';
+import { Button } from 'antd';
 import { TransactionRawDetails } from '../../models';
 
 /* eslint-disable-next-line */
 export interface EntryFormProps {
   transactionDetails: Partial<TransactionRawDetails>;
   onEntrySubmit: (values: Partial<TransactionRawDetails>) => void;
+  buttonLabel?: string;
 }
 
-export function EntryForm({ transactionDetails, onEntrySubmit }: EntryFormProps) {
+export function EntryForm({ transactionDetails, onEntrySubmit, buttonLabel = 'Submit' }: EntryFormProps) {
   return (
     <div>
       <Formik onSubmit={onEntrySubmit} initialValues={transactionDetails}>
@@ -18,6 +20,7 @@ export function EntryForm({ transactionDetails, onEntrySubmit }: EntryFormProps)
           <HacketInput name='journalNumber' placeholder='Journal Number' />
           <HacketInput name='remarks' placeholder='Remarks'/>
           <HacketInput name='date' placeholder='date'/>
+          <Button type='primary' htmlType='submit'>{buttonLabel}</Button>
         </Form>
       </Formik>
     </div>
