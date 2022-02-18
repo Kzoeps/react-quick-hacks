bb9b8480fb7f64e96bfb26a601f0d8e60c1a5fd4
kzoeps <kzoepa@gmail.com>
2022-02-18 20:34:28 +0600

    feat(ui-kit): create and export hacket table

---
+++ libs/ui-kit/src/lib/components/hacket-table/hacket-table.tsx
@@ -0,0 +1,14 @@
+import './hacket-table.module.scss';
+
+/* eslint-disable-next-line */
+export interface HacketTableProps {}
+
+export function HacketTable(props: HacketTableProps) {
+  return (
+    <div>
+      <h1>Welcome to hacket-table!</h1>
+    </div>
+  );
+}
+
+export default HacketTable;
--- libs/ui-kit/src/lib/components/index.ts
+++ libs/ui-kit/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export * from './hacket-input/hacket-input';
 export * from './hacket-select/hacket-select';
 export * from './hacket-upload/hacket-upload';
+export * from './hacket-table/hacket-table';
