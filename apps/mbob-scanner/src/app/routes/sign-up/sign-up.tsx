c8f21ef24ed9ebfe9ea46055b235f981cfaa0f53
kzoeps <kzoepa@gmail.com>
2022-02-22 11:57:29 +0600

    feat(sign up): implement loader for signup

--- apps/mbob-scanner/src/app/routes/sign-up/sign-up.tsx
+++ apps/mbob-scanner/src/app/routes/sign-up/sign-up.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SignUpControlNames, SignUpForm, SignUpFormValues } from '@react-quick-hacks/auth';
 import { usePhoneVerify } from '@react-quick-hacks/firebase-auth';
-import { getFirestore, doc, setDoc } from "firebase/firestore"
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { appendBhtCode, NotificationTypeEnum, showNotification } from '@react-quick-hacks/shared';
 import { useNavigate } from 'react-router-dom';
 import app from '../../firebase-config';
 import { RoutesEnum } from '../../enums/routes-enum';
+import { Spin } from 'antd';
 
 /* eslint-disable-next-line */
 export interface SignUpProps {}
 
 export function SignUp(props: SignUpProps) {
   const [showOtp, setShowOtp] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);
   const DISABLED_CONTROLS: SignUpControlNames[] = ['phoneNumber', 'name', 'dzongkhag'];
   const verifyPhone = usePhoneVerify(app);
   const db = getFirestore(app);
   const navigate = useNavigate();
   const generateOtp = async (signUpForm: SignUpFormValues) => {
+    setShowLoader(true);
     const { phoneNumber } = signUpForm;
     await verifyPhone.sendVerification(`+975${phoneNumber}`);
+    setShowLoader(false);
     setShowOtp(true);
   };
 
@@ -35,24 +39,29 @@ export function SignUp(props: SignUpProps) {
     try {
       const { otp } = signUpForm;
       if (otp) {
+        setShowLoader(true);
         const signedIn = await verifyPhone.verifyOtp(otp)
         if (signedIn) {
           await setUpUserProfile(signUpForm);
           showNotification('Thanks for signing up', NotificationTypeEnum.Success);
-          navigate(`/${RoutesEnum.dashboard}`, {replace: true});
+          navigate(`/${RoutesEnum.dashboard}`, { replace: true });
         }
       }
     } catch (e) {
       showNotification(e.message, NotificationTypeEnum.Error);
     }
   };
-  return (
+
+  useEffect(() => () => {
+    setShowLoader(false);
+  });
+  return showLoader ? <Spin /> : (
     <>
       <SignUpForm
-        onSubmit={showOtp ? verifyOtp :generateOtp}
+        onSubmit={showOtp ? verifyOtp : generateOtp}
         showOtpEntry={showOtp}
         buttonLabel={showOtp ? 'Sign Up' : 'Generate OTP'}
-        controlsToDisable={showOtp ? DISABLED_CONTROLS : []}/>
+        controlsToDisable={showOtp ? DISABLED_CONTROLS : []} />
       <a href={`/${RoutesEnum.login}`}>Login</a>
       <div id='recaptcha-container' />
     </>
